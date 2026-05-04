4fa66e8280f454c93e979f6b5f771ff0335610b9
nightsleep1023 <nightdes921023@gmail.com>
2026-05-05 01:11:19 +0800

    測試可不可以push

---
+++ OneStarProblem/UVA105_The_Skyline_Problem.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+
+int main(){
+    int r=0;
+    int h=0;
+    int l=0;
+    int max=0;
+    int min=10000;
+    int array[10005] = {0};
+    while(scanf("%d %d %d",&r,&h,&l)!=EOF){
+        if(min>r){
+            min = r;
+        }
+        if(max<l){
+            max = l;
+        }
+        for(int i=r;i<l;i++){
+            if(array[i]<h){
+                array[i] = h;
+            }
+        }
+    }
+    h=0;
+    for(int i=min;i<=max;i++){
+        if(i!=min){
+            printf(" ");
+        }
+        if(array[i]!=h){
+            printf("%d %d",i,array[i]);
+        }
+    }
+}
